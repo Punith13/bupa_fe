7ae0164fe7bac7495a96cc68df4d1be76cc423d4
Punith Nayak <punithnayak@Puniths-Mac-Studio.local>
2023-11-07 22:52:58 +1100

    interim commit

--- README.md
+++ README.md
@@ -8,19 +8,22 @@ Before you can run the Vite app and the server, please make sure you have the fo
 
 - Node.js: You can download and install Node.js from [nodejs.org](https://nodejs.org/).
 
-## Running the Vite App
+## Start Dev Application server - Frontend
 
 1. Open a terminal and navigate to the project's root directory.
 
 2. Install project dependencies by running the following command:
 
-`npm install`
+```
+cd client
+npm install
+```
 
 3. Start the Vite application by running the following command:
 
 `npm run dev`
 
-This command will start the development server for the Vite application. You can access the app in your web browser at `http://localhost:3000`.
+This command will start the development server for the Vite application. You can access the app in your web browser at `http://localhost:5173`.
 
 4. To build the Vite application for production, you can use the following command:
 
@@ -42,22 +45,39 @@ This will create an optimized production build in the `dist` directory.
 
 `npm run start`
 
-This will start the server, and it should be accessible at `http://localhost:8080`.
+This will start the server, and it should be accessible at `http://localhost:4000`.
+
+## Docker 
+
+The application can also be run using docker. A nginx container splits traffic between the Front end and Graphql API requests
+To build and access the application , run the following commands 
+
+```
+docker-compose build
+docker-compose up
+```
+
+Website is available at localhost
+
 
 ## Running Tests
 
-To run tests for the server, you can use the following command:
+To run tests for the Front end client application, from the root directory you can use the following command:
 
-`npm run test`
+```
+cd client 
+npm run test
+```
+
+To run tests for the server application, from the root directory you can use the following command:
+
+```
+cd server
+npm run test
+```
 
 This command will execute the tests for the server and provide feedback on the test results.
 
-## Additional Information
+## License
 
-- You can customize the Vite app by editing the files in the project's root directory, including `src/` for your application code.
-
-- For server configuration and code, you can explore the `server/` directory.
-
-- Make sure to refer to the documentation and README files in both the root project and the `server` directory for more detailed information on configuration and usage.
-
-That's it! You should now be able to run the Vite app and the server for your project. If you encounter any issues, please check the error messages in the terminal for guidance on resolving them.
+[MIT](https://choosealicense.com/licenses/mit/)
--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -25,45 +25,54 @@ const Main = () => {
     return bookTitles.sort();
   };
 
-  return loading ? (
-    <Loader />
-  ) : error ? (
-    <p data-testid="error">Unable to load the data, please try again!</p>
-  ) : (
+  return (
     <>
       <Header />
-      <div className="flex flex-col sm:w-[35%] w-[80%] mx-auto">
-        <div className="flex flex-col sm:flex-row gap-4 my-10 justify-center items-center sm:items-start flex-wrap">
-          <Book
-            hardCover={hardCover}
-            bookTitles={getBookTitles(data?.people, "Male")}
-            gender="Male"
-          />
-          <Book
-            hardCover={hardCover}
-            bookTitles={getBookTitles(data?.people, "Female")}
-            gender="Female"
-          />
-        </div>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <p
+          data-testid="error"
+          className="mt-10 text-center text-xl text-red-400"
+        >
+          Unable to load the data, please try again!
+        </p>
+      ) : (
+        <>
+          <div className="flex flex-col sm:w-[35%] w-[80%] mx-auto">
+            <div className="flex flex-col sm:flex-row gap-4 my-10 justify-center items-center sm:items-start flex-wrap">
+              <Book
+                hardCover={hardCover}
+                bookTitles={getBookTitles(data?.people, "Male")}
+                gender="Male"
+              />
+              <Book
+                hardCover={hardCover}
+                bookTitles={getBookTitles(data?.people, "Female")}
+                gender="Female"
+              />
+            </div>
 
-        <hr className="border-2 bg-gray-300 " />
+            <hr className="border-2 bg-gray-300 " />
 
-        <div className="mt-2 flex flex-col-reverse sm:flex-row sm:justify-end ">
-          <button
-            className=" mr-1 mb-1 px-3 py-1 bg-transprent text-md font-bold text-bupa_blue underline underline-offset-4"
-            onClick={() => setHardCover(true)}
-          >
-            Hardcover only
-          </button>
-          <button
-            type="button"
-            className="text-white bg-bupa_blue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onClick={() => setHardCover(false)}
-          >
-            Get Books
-          </button>
-        </div>
-      </div>
+            <div className="mt-2 flex flex-col-reverse sm:flex-row sm:justify-end ">
+              <button
+                className=" mr-1 mb-1 px-3 py-1 bg-transprent text-md font-bold text-bupa_blue underline underline-offset-4"
+                onClick={() => setHardCover(true)}
+              >
+                Hardcover only
+              </button>
+              <button
+                type="button"
+                className="text-white bg-bupa_blue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={() => setHardCover(false)}
+              >
+                Get Books
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
